b8a71399a3b50bdc68d06d67f3f0521c5fbd54b1
Marcin Deptuła <marcin.deptula@live.com>
2014-09-01 02:27:03 +0200

    Added better support for loading assemblies by plugins

--- Ultima.Core/PluginManager.fs
+++ Ultima.Core/PluginManager.fs
@@ -1,6 +1,7 @@
 ﻿namespace Ultima
 
 open System
+open System.IO
 open System.Threading
 open System.Threading.Tasks
 open System.Reflection
@@ -16,6 +17,24 @@ type PluginManager() =
     let configExtensions = [".json"; ".cfg"; ".config"]
     let runningPlugins = List<IPlugin * Task>()
 
+    do
+        AppDomain.CurrentDomain.add_AssemblyResolve(
+            ResolveEventHandler(fun _ y ->
+                try
+                    let p = Directory.EnumerateFiles(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "plugins"), "*.dll")
+                            |> Seq.tryFind(fun m ->
+                                let asmName = AssemblyName(y.Name).Name
+                                if (Path.GetFileNameWithoutExtension(m) = asmName) then
+                                    log.Info "Manually loaded Assembly %s" y.Name
+                                    true
+                                else
+                                    false)
+                    match p with
+                    | Some asm -> Assembly.LoadFrom asm
+                    | None -> null
+                with
+                | _ -> null))
+
     let LoadPlugin(pluginPath: string) = 
         let asm = Assembly.LoadFile(pluginPath)
         let t = asm.GetTypes()
@@ -27,7 +46,7 @@ type PluginManager() =
         | None -> log.Info "%s is not an Ultima plugin" pluginPath
                   None
 
-    let HookTaskEnd (plugin: IPlugin, task: Task) = 
+    let HookTaskEnd (plugin: IPlugin, task: Task, status: IDisposable) = 
         task.ContinueWith( (fun (t: Task) -> 
             let wf = async {
                         do! Async.SwitchToActor(context)
@@ -37,6 +56,7 @@ type PluginManager() =
                         | exn -> log.Error "Plugin %s finished with exception: %O" (plugin.Info.Name) exn
 
                         runningPlugins.Remove( (plugin, task) ) |> ignore
+                        status.Dispose()
                      }
             Async.StartImmediate wf) 
         ) |> ignore
@@ -68,12 +88,19 @@ type PluginManager() =
             | Some plugin ->
                 let pluginCfg = TryLoadPlugin pluginPath configExtensions
                 plugin.Initialize(services, pluginCfg)
+                let status = plugin.StatusChanged.Subscribe((fun s -> log.Info "Plugin %s changed status: %s" plugin.Info.Name s),
+                                                            (fun (exc: exn) -> log.Error "Plugin %s signaled error through status %O" plugin.Info.Name exc))
+                
                 let task = plugin.Run()
                 log.Info "Plugin %s (%s) running" (plugin.Info.Name) (plugin.Info.Version.ToString())
-                (plugin, task) |> HookTaskEnd
+
+                (plugin, task, status) |> HookTaskEnd
                 runningPlugins.Add( (plugin, task) )
                 s := true
         with
+        | :? ReflectionTypeLoadException as exn -> 
+                log.Error "Cannot start plugin %s because exception was thrown: %O\r\n\r\nType load exceptions: %A"
+                    pluginPath exn (List.ofArray exn.LoaderExceptions)
         | exn -> log.Error "Cannot start plugin %s because exception was thrown: %O"
                     pluginPath exn
         return !s
